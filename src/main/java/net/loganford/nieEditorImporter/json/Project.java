ef03c11374f5c314a1264af91ba2b2059eec37cc
logan.ford <retrogamer500@gmail.com>
2024-04-02 22:01:26 -0400

    Adding project properties

--- src/main/java/net/loganford/nieEditorImporter/json/Project.java
+++ src/main/java/net/loganford/nieEditorImporter/json/Project.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Project {
@@ -13,6 +14,7 @@ public class Project {
     protected List<Tileset> tilesets = new ArrayList<>();
     protected List<EntityDefinition> entityDefinitions = new ArrayList<>();
     @Getter @Setter private List<Room> rooms = new ArrayList<>();
+    @Getter @Setter private HashMap<String, String> properties = new HashMap<>();
 
     public Room getRoom(String roomName) {
         return rooms.stream().filter(r -> roomName.equals(r.getName())).findFirst().orElse(null);
